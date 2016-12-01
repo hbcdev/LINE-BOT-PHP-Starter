3d6f1a1635852c4a153a45de51108ba8ef783f48
Wacharakiat Arkachaisri <wacharakiat@hbc-ad.local>
2016-12-01 20:52:17 +0700

    commit #4

--- bot.php
+++ bot.php
@@ -1,5 +1,9 @@
 <?php
 $access_token = '+ecG0yQsz6cEwRLzk1xVb8w6UxsfsuzBedcochChB3Qs1HSsbt2NPCmUgSUQmdozqr0Br0brofWZ/dG3+A/fKwY3y5w1S3E7vMzxkLn9yZn3wIWpAzy8+Z1kv+ke17cRUe8IpyNyFhhXYpT7/wRN5gdB04t89/1O/w1cDnyilFU=';
+$proxy = 'velodrome.usefixie.com:80'
+$proxyauth = 'fixie:lE4BKQBXEJwHZCU'
+
+
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -37,6 +41,8 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($ch, CURLOPT_PROXY, $proxy);
+            curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 			$result = curl_exec($ch);
 			curl_close($ch);
 
--- verify.php
+++ verify.php
@@ -1,5 +1,7 @@
 <?php
 $access_token = '+ecG0yQsz6cEwRLzk1xVb8w6UxsfsuzBedcochChB3Qs1HSsbt2NPCmUgSUQmdozqr0Br0brofWZ/dG3+A/fKwY3y5w1S3E7vMzxkLn9yZn3wIWpAzy8+Z1kv+ke17cRUe8IpyNyFhhXYpT7/wRN5gdB04t89/1O/w1cDnyilFU=';
+$proxy = 'velodrome.usefixie.com:80'
+$proxyauth = 'fixie:lE4BKQBXEJwHZCU'
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
@@ -9,6 +11,8 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 $result = curl_exec($ch);
 curl_close($ch);
 
