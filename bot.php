0f572f3ffd2f794a7e54e7d4b7e889e123de0240
Wacharakiat Arkachaisri <wacharakiat@hbc-ad.local>
2016-12-02 08:49:26 +0700

    change bot.php

--- bot.php
+++ bot.php
@@ -6,9 +6,9 @@ $proxyauth = 'fixie:lE4BKQBXEJwHZCU';
 
 
 // Get POST body content
-//$content = file_get_contents('php://input');
+$content = file_get_contents('php://input');
 
-$content = parse_str(file_get_contents("php://input"), $_POST);
+//$content = parse_str(file_get_contents("php://input"), $_POST);
 
 // Parse JSON
 $events = json_decode($content, true);
